0bd7427cd8110d6aff1b8cdbe69b027b78b6572f
JuPalmas <ahiizew@gmail.com>
2021-06-22 10:53:48 -0400

    Formulario de registro

--- web/views.py
+++ web/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 def index(request):
     return render(request, 'web/index.html', {})
+
+def register(request):
+    data = {
+        'form':CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"],password=formulario.cleaned_data["password1"])
+            usernames=formulario.cleaned_data["username"]
+            messages.success(request,f"{usernames}")
+            login(request, user)
+            return redirect(to="index")
+        data["form"] = formulario
+    return render(request,'registration/register.html',data)
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username' ,'first_name','last_name',"email","password1","password2"]
