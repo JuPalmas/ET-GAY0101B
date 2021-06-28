4bb5994c0f8aa7e94acb79ad07ee0ffce948e6af
FelipeMunozz <fi.munozl@alumnos.duoc.cl>
2021-06-27 21:16:27 -0400

    Cambio modelo post , dejo funcionando el publicar
    Signed-off-by: FelipeMunozz <fi.munozl@alumnos.duoc.cl>

--- web/models.py
+++ web/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    autor = models.CharField(max_length=200)
     titulo = models.CharField(max_length=200)
     precio = models.IntegerField()
     imagen = models.ImageField(null=True)
